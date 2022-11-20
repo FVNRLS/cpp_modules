bf594ba667da167b49fdbcf7cdbb4b48ea2b882f
FVNRLS <rmazurit@student.42heilbronn.de>
2022-11-20 14:00:39 +0100

    ex1 start

--- CPP_00/ex01/contacts.cpp
+++ CPP_00/ex01/contacts.cpp
@@ -14,8 +14,21 @@
 #include <string>
 
 int main(int argc, char **argv) {
+	int i;
 
-
+	if (argc == 1){
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE * " << std::endl;
+		return (EXIT_SUCCESS);
+	}
+	i = 1;
+	while (argv[i] != nullptr)
+	{
+		for (int j = 0; j < (int)strlen(argv[i]); j++)
+			std::cout << (unsigned char)toupper(argv[i][j]);
+		std::cout << " ";
+		i++;
+	}
+	std::cout << std::endl;
 
 	return (EXIT_SUCCESS);
 }
