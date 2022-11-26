80de2fdf395f6225424fc9f8b0eff57f35c813f2
FVNRLS <rmazurit@student.42heilbronn.de>
2022-11-26 13:29:45 +0100

    ex04 finished

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -14,105 +14,94 @@
 #include <string>
 #include <fstream>
 
-void	get_input(std::string &infile, std::string &s1, std::string &s2) {
-	while (infile.length() == 0)
-	{
-		std::cout << "Enter the filename:		";
-		std::getline(std::cin, infile);
-		if (std::cin.eof())
-			return ;
+static int	get_input(std::string &infile, std::string &s1, std::string &s2, int argc, char **argv) {
+	if (argc != 4) {
+		std::cout << "error: invalid number of arguments" << std::endl;
+		return (EXIT_FAILURE);
 	}
-	while (s1.length() == 0)
-	{
-		std::cout << "Enter the first string:		";
-		std::getline(std::cin, s1);
-		if (std::cin.eof())
-			return ;
+	infile = argv[1];
+	if (infile.length() == 0) {
+		std::cout << "error: missing input file" << std::endl;
+		return (EXIT_FAILURE);
 	}
-	while (s2.length() == 0)
-	{
-		std::cout << "Enter the second string:	";
-		std::getline(std::cin, s2);
-		if (std::cin.eof())
-			return ;
+	s1 = argv[2];
+	if (s1.length() == 0) {
+		std::cout << "error: missing string to replace" << std::endl;
+		return (EXIT_FAILURE);
 	}
+	s2 = argv[3];
+	if (s2.length() == 0) {
+		std::cout << "error: missing string to replace with" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
 
-std::string	replace_line(std::string &line, std::string &s1, std::string &s2) {
-	std::string result = "Hello";
+static bool	open_error(std::ifstream &infile, std::ofstream &outfile, std::string &in_name, std::string &out_name) {
+	if (!infile.is_open()) {
+		std::cout << "error: failed to open " << in_name << std::endl;
+		return (true);
+	}
+	if (!outfile.is_open()) {
+		std::cout << "error: failed to create " << out_name << std::endl;
+		infile.close();
+		return (true);
+	}
+	return (false);
+}
+
+static std::string	replace_line(std::string &line, std::string &s1, std::string &s2) {
+	std::string	result;
 	size_t 		start_pos;
-	size_t 		pos;
+	size_t 		i;
+	size_t		s1_len;
+	size_t		line_Len;
 
+	s1_len = s1.length();
+	line_Len = line.length();
+	i = 0;
 	start_pos = 0;
-
-
+	while ((i = line.find(s1, i)) != std::string::npos) {
+		result += line.substr(start_pos, i - start_pos);
+		result += s2;
+		i += s1_len;
+		start_pos = i;
+	}
+	if (start_pos < line_Len)
+		result += line.substr(start_pos, line_Len - start_pos);
 	return (result);
 }
 
-void	replace_content(std::ifstream &infile, std::ofstream &outfile, std::string &s1, std::string &s2) {
+static void	replace_content(std::ifstream &infile, std::ofstream &outfile, std::string &s1, std::string &s2) {
 	std::string line;
 	std::string result;
 
 	while (std::getline(infile, line)) {
 		result = replace_line(line, s1, s2);
-		outfile << result << std::endl;
+		if (infile.eof())
+			outfile << result;
+		else
+			outfile << result << std::endl;
 	}
 }
 
-int	main() {
+int	main(int argc, char **argv) {
 	std::string	in_name;
 	std::string out_name;
-//	std::string	s1;
-//	std::string	s2;
+	std::string	s1;
+	std::string	s2;
 
-//	get_input(in_name, s1, s2);
-//	if (std::cin.eof())
-//		return (0);
-//
-//	out_name = in_name + ".replace";
-
-	std::string str1 = "blablaFINDblablaFIND";
-	std::string	str2 = "FIND";
-	std::string str3 = "REPLACE";
-	std::string sub;
-	std::string result;
-
-	size_t 	i;
-	size_t	start_pos;
-
-	i = 0;
-	start_pos = 0;
-	while ((i = str1.find(str2, i)) != std::string::npos) {
-		std::cout << i << std::endl;
-		sub = str1.substr(start_pos, i - start_pos);
-		result += sub;
-		result += str3;
-		i += str2.length();
-		start_pos = i;
-		std::cout << "start pos:	" << start_pos << std::endl;
-	}
-	std::cout << result << std::endl;
-
+	if (get_input(in_name, s1, s2, argc, argv) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	out_name.append(in_name + ".replace");
 
 	std::ifstream infile(in_name);
-	std::ofstream outfile(out_name);
+	std::ofstream outfile(out_name, std::ios_base::trunc);
+	if (open_error(infile, outfile, in_name, out_name))
+		return (EXIT_FAILURE);
 
-
-
+	replace_content(infile, outfile, s1, s2);
 	infile.close();
-
-
 	outfile.close();
-
-
-	return (0);
+	return (EXIT_SUCCESS);
 }
-
-//	std::fstream infile(in_name, std::ios::in);
-//	std::fstream outfile(out_name, std::ios::out | std::ios::trunc);
-
-//	infile.open(in_name, std::ios::in);
-//	outfile.open("test.txt", std::ios::out | std::ios::trunc);
-//
-//	if (infile.is_open() && outfile.is_open())
-//		std::cout << "bljajajajaja";
