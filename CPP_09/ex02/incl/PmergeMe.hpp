6659557d082f071ebc67f7bc6a3a089a78909297
FVNRLS <rmazurit@student.42heilbronn.de>
2023-03-12 17:55:56 +0100

    ex02 basic class setup / ex01 headers cleaned up

--- CPP_09/ex02/incl/PmergeMe.hpp
+++ CPP_09/ex02/incl/PmergeMe.hpp
@@ -1,37 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmazurit <rmazurit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/12 12:54:13 by rmazurit          #+#    #+#             */
-/*   Updated: 2023/03/12 12:54:13 by rmazurit         ###   ########.fr       */
+/*   Created: 2023/03/12 17:51:47 by rmazurit          #+#    #+#             */
+/*   Updated: 2023/03/12 17:51:47 by rmazurit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
-#include <list>
+#include <vector>
+#include <map>
 
-class RPN {
+enum errors {
+
+};
+
+class PmergeMe {
 
 private:
-	std::list<char>	_rpn_data;
-	std::list<char>	_norm_data;
-	long 			_res;
-	std::string 	_input;
-
-	int 			parse_input();
-	int 			normalize_rpn();
 
 public:
-	RPN();
-	RPN(const std::string &input);
-	RPN(const RPN &src);
-	RPN &operator=(const RPN &src);
-	~RPN();
 
-	int calculate();
 };
--- CPP_09/ex02/src/PmergeMe.cpp
+++ CPP_09/ex02/src/PmergeMe.cpp
@@ -1,37 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmazurit <rmazurit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/12 12:54:13 by rmazurit          #+#    #+#             */
-/*   Updated: 2023/03/12 12:54:13 by rmazurit         ###   ########.fr       */
+/*   Created: 2023/03/12 17:51:31 by rmazurit          #+#    #+#             */
+/*   Updated: 2023/03/12 17:51:31 by rmazurit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
-
-#include <iostream>
-#include <list>
-
-class RPN {
-
-private:
-	std::list<char>	_rpn_data;
-	std::list<char>	_norm_data;
-	long 			_res;
-	std::string 	_input;
-
-	int 			parse_input();
-	int 			normalize_rpn();
-
-public:
-	RPN();
-	RPN(const std::string &input);
-	RPN(const RPN &src);
-	RPN &operator=(const RPN &src);
-	~RPN();
-
-	int calculate();
-};
